7a72040b27b4c0de94569aae005ce7ff9914a85d
faruk musa <farookmousa2017@gmail.com>
2025-07-08 10:27:40 +0300

    İlk yükleme

---
+++ msp430g2xx1_1.c/Debug/msp430g2xx1_1.d
@@ -0,0 +1,21 @@
+# FIXED
+
+msp430g2xx1_1.obj: ../msp430g2xx1_1.c
+msp430g2xx1_1.obj: C:/ti/ccs2020/ccs/ccs_base/msp430/include/msp430.h
+msp430g2xx1_1.obj: C:/ti/ccs2020/ccs/ccs_base/msp430/include/msp430g2231.h
+msp430g2xx1_1.obj: C:/ti/ccs2020/ccs/ccs_base/msp430/include/in430.h
+msp430g2xx1_1.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+msp430g2xx1_1.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../msp430g2xx1_1.c:
+
+C:/ti/ccs2020/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2020/ccs/ccs_base/msp430/include/msp430g2231.h:
+
+C:/ti/ccs2020/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
---
+++ msp430g2xx1_1.c/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2231.cmd 
+
+C_SRCS += \
+../msp430g2xx1_1.c 
+
+C_DEPS += \
+./msp430g2xx1_1.d 
+
+OBJS += \
+./msp430g2xx1_1.obj 
+
+OBJS__QUOTED += \
+"msp430g2xx1_1.obj" 
+
+C_DEPS__QUOTED += \
+"msp430g2xx1_1.d" 
+
+C_SRCS__QUOTED += \
+"../msp430g2xx1_1.c" 
+
+
